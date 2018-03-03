30fe8f735f2073d9a8ed16cee64b879535b8d482
Neoh213 <djrumph@gmail.com>
2018-03-03 00:27:10 -0500

    updates

--- project/test.py
+++ project/test.py
@@ -7,7 +7,7 @@ store = 'Legros and Sons'
 #sound_level = 0
 
 for x in range(0,100):
-    print (randint(1,100))
+    print (randint(1,100) + 0.2)
     sys.stdout.flush()
     #sound_level = sound_level+1
     time.sleep(.5);
