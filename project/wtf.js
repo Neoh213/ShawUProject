b2ca534609fbec53f044249e04c027422b00ccf9
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2018-06-29 10:57:28 -0400

    Update wtf.js

--- project/wtf.js
+++ project/wtf.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'DJ',
   database : 'practice',
-  password : 'Neoheartless1'   //your root user's password
+  password : ''   //your root user's password
 });
 
 
